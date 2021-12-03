a0fc629deb4da9fb85544044d14f6b856ee451be
= <=>
2021-12-03 11:16:03 +0400

    Client-2 view

---
+++ Client_2/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using Client_2.Models;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client_2.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = new List<Product>();
+            using (HttpClient client = new HttpClient())
+            {
+                var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+                
+                client.SetBearerToken(accessToken);
+
+                var response = await client.GetAsync("https://localhost:5007/products/getproducts/");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    products = JsonConvert.DeserializeObject<List<Product>>(content);
+                    return View(products);
+                }
+                
+            }
+            return View(products);
+        }
+    }
+}
