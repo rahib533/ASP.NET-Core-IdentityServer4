de5f209f8588173aa2ce7df4084142dd8a48ccd6
= <=>
2021-12-05 12:16:26 +0400

    CustomProfileService.cs

---
+++ AngularAppWithIdentityServer/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private authService:AuthService) { }
+  status = "";
+  ngOnInit(): void {
+    this.authService.userManager.getUser().then((user) =>{
+      if(user){
+        console.log(user);
+        this.status = "Welcome";
+      }
+      else{
+        this.status = "Not Log in";
+      }
+    });
+  }
+  login() : void{
+    this.authService.userManager.signinRedirect();
+  }
+  logout() : void{
+    this.authService.userManager.signoutRedirect();
+  }
+}
